7b7cbb377c6b787c04d54c9eb9d069951d7db868
Samanvai Chandra <83567036+notorious1337@users.noreply.github.com>
2025-04-04 09:34:11 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,72 +1,41 @@
-#define SIZE 50     /* Size of Stack */ 
-#include <ctype.h>
-#include <stdio.h> 
-char s[SIZE];
-int top = -1;      /* Global declarations */
-void push(char elem)     /* Function for PUSH operation */
-{	
-	s[++top] = elem;
-}
- 
-char pop()     /* Function for POP operation */
+#include<stdio.h>
+void main()
 {
-	return (s[top--]);
-}
- 
-int pr(char elem)   /* Function for precedence */
+int 
+n,v,u,cost[10][10],parent[10]={0},i,j;
+int count=1,mincost=0,min,a,b;
+printf("\n enter no of vertices");
+scanf("%d",&n);
+printf("\n enter cost matrix");
+for(i=1;i<=n;i++)
+for(j=1;j<=n;j++)
 {
-	switch (elem)
-	{
-		case '#':
-				return 0; 
-		case '(': 
-				return 1;
-		case '+':
-		case '-': 
-				return 2;
-		case '*': 
-		case '/': 
-		case '%': 
-				return 3; 
-		case '^':
-				return 4;
-	}
- 
+scanf("%d",&cost[i][j]);
+if(cost[i][j]==0)
+cost[i][j]=999;
 }
- 
-void main()   /* Main Program */
+while(count<n)
+{    min=999;
+for(i=1;i<=n;i++)
+for(j=1;j<=n;j++)
+if(cost[i][j]<min)
 {
-	char infx[50], pofx[50], ch, elem; 
-	int i = 0, k = 0;
-	printf("\n\nEnter the Infix Expression "); 
-	scanf("%s", infx);
-	push('#');
-	while ((ch = infx[i++]) != '\0')
-	{
-		if (ch == '(')
-			push(ch);
-		else if (isalnum(ch)) 
-			pofx[k++] = ch; 
-		else if (ch == ')')
-		{
-			while (s[top] != '(') 
-				pofx[k++] = pop();
-			elem = pop();            /* Remove ( */
-		}
-		else	/* Operator */
-		{
-			while (pr(s[top]) >= pr(ch)) 
-				pofx[k++] = pop();
-			push(ch);
- 
-		}
- 
-	}
- 
-	while (s[top] != '#')        /* Pop from stack till empty */ 
-		pofx[k++] = pop();
-    	pofx[k] = '\0'; /* Make pofx as valid string */
-	printf("\n\nGiven Infix Expn: %s Postfix Expn: %s\n", infx, pofx);
+min=cost[i][j];
+a=u=i;
+b=v=j;
 }
- 
- 
+while(parent[u])
+u=parent[u];
+while(parent[v])
+v=parent[v];
+if(u!=v)
+{
+count++;
+printf("\n edge(%d,%d)=%d",a,b,min);
+mincost+=min;
+parent[v]=u;
+}
+cost[a][b]=cost[b][a]=999;
+}  /*End of While */
+printf("\n minimum cost=%d",mincost);
+}

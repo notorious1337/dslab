649562172317c5722dcd288a306408dec4d610ca
Samanvai Chandra <83567036+notorious1337@users.noreply.github.com>
2025-05-08 22:32:42 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,80 +1,52 @@
-//quicksort
-#include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
-#define MAX 500000
-int partition(int a[],int low,int high);
-int swap(int *a,int *b)
+//prims
+#include <stdio.h>
+int a,b,u,n,i,j,ne=1,v;
+int visited[10],min,mincost=0,cost[10][10];
+int main()
 {
-	int temp;
-	temp=*a;
-	*a=*b;
-	*b=temp;
-	return temp;
-}
-
-void quicksort(int a[],int low,int high)
+printf("\n Enter the no of vertices & graph data : ");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
 {
-	int s;
-	if(low<high)
-	{
-		s=partition(a,low,high);
-		quicksort(a,low,s-1);
-		quicksort(a,s+1,high);
-	}
-}
-
-
-int partition(int a[],int low,int high)
+for(j=1;j<=n;j++)
 {
-	int  i,j,pivot;
-	pivot=a[low];
-	i=low+1;
-	j=high;
-	while(1)
-	{
-		while((i<high)&&(pivot>=a[i]))
-		i++;
-		while(pivot<a[j])
-		j--;
-		if(i<j)
-		{
-			swap(&a[i],&a[j]);
-		}
-		else
-		{
-			swap(&a[low],&a[j]);
-				       return j;
-		}
-	}
+printf("\n(%d,%d):",i,j);
+scanf("%d",&cost[i][j]);
+if(cost[i]==0)
+cost[i][j]=999;
 }
-
-
-void main()
+}
+for(i=0;i<=n;i++)
+visited[i]=0;
+printf("\n The edges of spanning tree are :\n");
+visited[1]=1;
+while(ne<n)
 {
- 	int a[MAX],n,i,low,high;
-  	clock_t start,end;
-     	float duration;
-     	printf("\n enter the number of elements to sort");
-     	scanf("%d", &n);
-     	printf("\n  elements are:\n");
-     	for(i=0;i<n;i++)
-     	{
-     		//scanf("%d",&a[i]);
-     		a[i]=random();
-		printf("Before Sorting\n");
-     		printf("%d\t",a[i]);
-     	}
-     	printf("\n");
-     	start=clock();
-     	low=0,high=n-1;
-     	quicksort(a,low,high);
-     	end=clock();
-     	duration=(end-start);
-     	printf("\n sorted array is::\n");
-     	for(i=0;i<n;i++)
-     		printf("\n\t%d",a[i]);
-     	printf("\n");
-     	printf("time taken is %f",duration);
+for(i=1,min=999;i<=n;i++)
+{
+for(j=1;j<=n;j++)
+{
+if(cost[i][j]<min)
+{
+if(visited[i]==0)
+continue;
+else
+{
+min=cost[i][j];
+a=u=i;
+b=v=j;
 }
-
+}
+}
+}
+if(visited[u]==0||visited[v]==0)
+{
+printf("\n %d edge (%d,%d) = %d",ne++,a,b,min);
+mincost+=min;
+visited[b]=1;
+}
+cost[a][b]=cost[b][a]=999;
+}
+printf("\n\n\n Minimum cost : %d",mincost);
+return 0;
+}

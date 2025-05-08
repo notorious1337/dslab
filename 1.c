a5f3cb68c471e39a084fc873ecca151678fce051
Samanvai Chandra <83567036+notorious1337@users.noreply.github.com>
2025-05-08 22:31:59 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,76 +1,42 @@
-//mergesort
+//kruskals
 #include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
-#include<time.h>
-#define MAX 300000
-void simple_merge(int a[],int low,int mid,int high);
-void merge_sort(int a[],int low,int high);
-int n;
-int main( )
+void main()
 {
-	int a[MAX],i=0;
-	double startTime,endTime;
-	printf("Enter the number of elements to sort\n");
-	scanf("%d",&n);
-	//To generate randomly
-	for(i=0;i<n;i++)
-		a[i]=rand();
-	printf("\nBefore Sorting:\n");
-	for(i=0;i<n;i++)
-		printf("%d\t",a[i]);
-	printf("\n");
-	
-	startTime=clock();
-	merge_sort(a,0,n-1);
-	endTime = clock();
-	printf("After Sorting:\n");
-	for(i=0;i<n;i++)
-		printf("%d\t",a[i]);
-	printf("\n");
-	printf("Time taken is %10.9f\n",(double)(endTime-startTime));
-	return 0;
+int 
+n,v,u,cost[10][10],parent[10]={0},i,j;
+int count=1,mincost=0,min,a,b;
+printf("\n enter no of vertices");
+scanf("%d",&n);
+printf("\n enter cost matrix");
+for(i=1;i<=n;i++)
+for(j=1;j<=n;j++)
+{
+scanf("%d",&cost[i][j]);
+if(cost[i][j]==0)
+cost[i][j]=999;
 }
-
-/*Function to sort sub arrays elements*/
-void simple_merge(int a[],int low,int mid,int high)
+while(count<n)
+{    min=999;
+for(i=1;i<=n;i++)
+for(j=1;j<=n;j++)
+if(cost[i][j]<min)
 {
-	int i=low,j=mid+1,k=low,c[MAX];
-	while(i<=mid && j<=high)
-	{
-		if(a[i]<a[j])
-		{
-			c[k]=a[i];
-			i++;
-			k++;
-		}
-		else
-		{
-			c[k]=a[j];
-			j++;
-			k++;
-		}
-	}
-	while(i<=mid)
-		c[k++]=a[i++];
-	while(j<=high)
-		c[k++]=a[j++];
-	for(i=low;i<=high;i++)
-		a[i]=c[i];
+min=cost[i][j];
+a=u=i;
+b=v=j;
 }
-
-
-/*Function to divide the array list */
-void merge_sort(int a[],int low,int high)
+while(parent[u])
+u=parent[u];
+while(parent[v])
+v=parent[v];
+if(u!=v)
 {
-	int mid;
-	if(low<high)
-	{
-		mid=(low+high)/2;
-		{
-			merge_sort(a,low,mid);
-			merge_sort(a,mid+1,high);
-		}
-	simple_merge(a,low,mid,high);
-	}
+count++;
+printf("\n edge(%d,%d)=%d",a,b,min);
+mincost+=min;
+parent[v]=u;
+}
+cost[a][b]=cost[b][a]=999;
+}  /*End of While */
+printf("\n minimum cost=%d",mincost);
 }

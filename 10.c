67f13099b29b417be86c772044e18f0ec4292234
Samanvai Chandra <83567036+notorious1337@users.noreply.github.com>
2025-05-08 22:29:45 +0530

    Update 10.c

--- 10.c
+++ 10.c
@@ -1,135 +1,79 @@
-#include <stdio.h>
-#include <stdlib.h>
- 
-struct node {
-    int info;
-    struct node *llink;
-    struct node *rlink;
-};
-typedef struct node* NODE;
- 
-// Function to insert a node into the BST
-NODE insert_node(int item, NODE root) {
-    NODE cur, temp, prev;
-    temp = (NODE)malloc(sizeof(struct node));
-    if (temp == NULL) {
-        printf("Memory allocation failed!\n");
-        return root;
-    }
-    temp->info = item;
-    temp->llink = temp->rlink = NULL;
- 
-    if (root == NULL)
-        return temp;
- 
-    prev = NULL;
-    cur = root;
-    while (cur != NULL) {
-        prev = cur;
-        if (item < cur->info)
-            cur = cur->llink;
-        else
-            cur = cur->rlink;
-    }
- 
-    if (item < prev->info)
-        prev->llink = temp;
-    else
-        prev->rlink = temp;
- 
-    return root;
+//quicksort
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#define MAX 500000
+int partition(int a[],int low,int high);
+int swap(int *a,int *b)
+{
+	int temp;
+	temp=*a;
+	*a=*b;
+	*b=temp;
+	return temp;
 }
- 
-// Function to traverse the BST in inorder
-void inorder(NODE root) {
-    if (root == NULL)
-        return;
-    inorder(root->llink);
-    printf("%d\t", root->info);
-    inorder(root->rlink);
+
+void quicksort(int a[],int low,int high)
+{
+	int s;
+	if(low<high)
+	{
+		s=partition(a,low,high);
+		quicksort(a,low,s-1);
+		quicksort(a,s+1,high);
+	}
 }
- 
-// Function to traverse the BST in preorder
-void preorder(NODE root) {
-    if (root == NULL)
-        return;
-    printf("%d\t", root->info);
-    preorder(root->llink);
-    preorder(root->rlink);
+
+
+int partition(int a[],int low,int high)
+{
+	int  i,j,pivot;
+	pivot=a[low];
+	i=low+1;
+	j=high;
+	while(1)
+	{
+		while((i<high)&&(pivot>=a[i]))
+		i++;
+		while(pivot<a[j])
+		j--;
+		if(i<j)
+		{
+			swap(&a[i],&a[j]);
+		}
+		else
+		{
+			swap(&a[low],&a[j]);
+				       return j;
+		}
+	}
 }
- 
-// Function to traverse the BST in postorder
-void postorder(NODE root) {
-    if (root == NULL)
-        return;
-    postorder(root->llink);
-    postorder(root->rlink);
-    printf("%d\t", root->info);
+
+
+void main()
+{
+ 	int a[MAX],n,i,low,high;
+  	clock_t start,end;
+     	float duration;
+     	printf("\n enter the number of elements to sort");
+     	scanf("%d", &n);
+     	printf("\n  elements are:\n");
+     	for(i=0;i<n;i++)
+     	{
+     		//scanf("%d",&a[i]);
+     		a[i]=random();
+		printf("Before Sorting\n");
+     		printf("%d\t",a[i]);
+     	}
+     	printf("\n");
+     	start=clock();
+     	low=0,high=n-1;
+     	quicksort(a,low,high);
+     	end=clock();
+     	duration=(end-start);
+     	printf("\n sorted array is::\n");
+     	for(i=0;i<n;i++)
+     		printf("\n\t%d",a[i]);
+     	printf("\n");
+     	printf("time taken is %f",duration);
 }
- 
-// Function to search for a key in BST
-NODE search(int item, NODE root) {
-    if (root == NULL || item == root->info)
-        return root;
- 
-    if (item < root->info)
-        return search(item, root->llink);
- 
-    return search(item, root->rlink);
-}
- 
-int main() {
-    int choice, item;
-    NODE root = NULL, temp;
- 
-    while (1) {
-        printf("\n1. Insert an element\n");
-        printf("2. Traverse the Tree (Preorder, Inorder, Postorder)\n");
-        printf("3. Search for an element\n");
-        printf("4. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
- 
-        switch (choice) {
-            case 1:
-                printf("Enter the item to be inserted: ");
-                scanf("%d", &item);
-                root = insert_node(item, root);
-                break;
- 
-            case 2:
-                if (root == NULL) {
-                    printf("Tree is empty\n");
-                } else {
-                    printf("\nInorder Traversal: ");
-                    inorder(root);
-                    printf("\nPreorder Traversal: ");
-                    preorder(root);
-                    printf("\nPostorder Traversal: ");
-                    postorder(root);
-                    printf("\n");
-                }
-                break;
- 
-            case 3:
-                printf("Enter the element to be searched: ");
-                scanf("%d", &item);
-                temp = search(item, root);
-                if (temp == NULL)
-                    printf("Element does not exist\n");
-                else
-                    printf("The element %d is found\n", temp->info);
-                break;
- 
-            case 4:
-                exit(0);
- 
-            default:
-                printf("Invalid choice\n");
-        }
-    }
- 
-    return 0;
-}
- 
- 

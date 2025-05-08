62f24716f9eb6e783441d7b70aa089324a0576ae
Samanvai Chandra <83567036+notorious1337@users.noreply.github.com>
2025-05-08 22:40:33 +0530

    Update 8.c

--- 8.c
+++ 8.c
@@ -1,167 +1,54 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
- 
-struct node {
-    struct node *prev;
-    int ssn, phno;
-    float sal;
-    char name[20], dept[10], desg[20];
-    struct node *next;
-} *first = NULL, *last = NULL, *temp = NULL, *temp2 = NULL;
- 
-int count = 0;
- 
-void create() {
-    int ssn, phno;
-    float sal;
-    char name[20], dept[10], desg[20];
- 
-    temp = (struct node *)malloc(sizeof(struct node));
-    if (temp == NULL) {
-        printf("Memory allocation failed!\n");
-        return;
-    }
- 
-    temp->prev = temp->next = NULL;
- 
-    printf("\nEnter ssn, name, department, designation, salary, and phone number of employee: ");
-    scanf("%d %s %s %s %f %d", &ssn, name, dept, desg, &sal, &phno);
- 
-    temp->ssn = ssn;
-    strcpy(temp->name, name);
-    strcpy(temp->dept, dept);
-    strcpy(temp->desg, desg);
-    temp->sal = sal;
-    temp->phno = phno;
- 
-    count++;
+//subset
+#include "stdio.h"
+#include "math.h"
+int i,j,k,set[10],d;
+int n;
+double pow(double,double);
+void subset(int [],int,int);
+
+int main()
+{
+	printf("\n Enter the no of Elements : ");
+	scanf("%d",&n);
+	printf("\n Enter the Elements \n");
+	for(i=0;i<n;i++)
+		scanf("%d",&set[i]);
+	printf("\n Enter the Sum : ");
+	scanf("%d",&d);
+	subset(set,n,d);
+	return 0;
 }
- 
-void insertbeg() {
-    create();
-    if (first == NULL) {
-        first = last = temp;
-    } else {
-        temp->next = first;
-        first->prev = temp;
-        first = temp;
-    }
+
+
+void subset(int set[],int n,int d)
+{
+	int flag=0,index[30][100],i,j,sum,k,p;
+	for(i=0;i<pow(2,n);i++)
+	{
+		k=i,sum=0;
+		for(j=0;j<=n;j++)
+		{
+			if(k&1)
+			{
+				index[i][j]=1;
+				sum+=set[j];
+			}
+			else
+			index[i][j]=0;
+			k=k>>1;
+		}
+		if(sum==d)
+		{
+			flag=1;
+			printf("\n Solutions Are \n");
+			for(j=0;j<n;j++)
+{
+				if(index[i][j]==1)
+				printf("\n\t %d",set[j]);
+			}
+		}
+	}
+
+if(flag==0)
+	printf("\n No solutions are found!!!");
 }
- 
-void insertend() {
-    create();
-    if (first == NULL) {
-        first = last = temp;
-    } else {
-        last->next = temp;
-        temp->prev = last;
-        last = temp;
-    }
-}
- 
-void displaybeg() {
-    temp2 = first;
-    if (temp2 == NULL) {
-        printf("List empty to display\n");
-        return;
-    }
-    printf("\nLinked list elements from beginning:\n");
-    while (temp2 != NULL) {
-        printf("%d %s %s %s %.2f %d\n", temp2->ssn, temp2->name, temp2->dept, temp2->desg, temp2->sal, temp2->phno);
-        temp2 = temp2->next;
-    }
-    printf("Number of employees = %d\n", count);
-}
- 
-void deleteend() {
-    if (last == NULL) {
-        printf("List is empty\n");
-        return;
-    }
- 
-    if (first == last) {
-        printf("%d %s %s %s %.2f %d\n", last->ssn, last->name, last->dept, last->desg, last->sal, last->phno);
-        free(last);
-        first = last = NULL;
-    } else {
-        temp2 = last->prev;
-        temp2->next = NULL;
-        printf("%d %s %s %s %.2f %d\n", last->ssn, last->name, last->dept, last->desg, last->sal, last->phno);
-        free(last);
-        last = temp2;
-    }
-    count--;
-}
- 
-void deletebeg() {
-    if (first == NULL) {
-        printf("List is empty\n");
-        return;
-    }
- 
-    temp = first;
-    if (first == last) {
-        first = last = NULL;
-    } else {
-        first = first->next;
-        first->prev = NULL;
-    }
- 
-    printf("%d %s %s %s %.2f %d\n", temp->ssn, temp->name, temp->dept, temp->desg, temp->sal, temp->phno);
-    free(temp);
-    count--;
-}
- 
-int main() {
-    int ch, n, i;
- 
-    printf("-----------------MENU--------------------\n");
-    printf("1 - Create a DLL of n employees\n");
-    printf("2 - Display from beginning\n");
-    printf("3 - Insert at end\n");
-    printf("4 - Delete at end\n");
-    printf("5 - Insert at beginning\n");
-    printf("6 - Delete at beginning\n");
-    printf("7 - Exit\n");
-    printf("------------------------------------------\n");
- 
-    while (1) {
-        printf("\nEnter choice: ");
-        scanf("%d", &ch);
- 
-        switch (ch) {
-            case 1:
-                printf("\nEnter number of employees: ");
-                scanf("%d", &n);
-                for (i = 0; i < n; i++) {
-                    insertend();
-                }
-                break;
-            case 2:
-                displaybeg();
-                break;
-            case 3:
-                insertend();
-                break;
-            case 4:
-                deleteend();
-                break;
-            case 5:
-                insertbeg();
-                break;
-            case 6:
-                deletebeg();
-                break;
-            case 7:
-                exit(0);
-            default:
-                printf("Wrong choice\n");
-        }
-    }
- 
-    return 0;
-}
- 
- 
- 
